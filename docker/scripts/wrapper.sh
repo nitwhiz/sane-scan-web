1d6f0daad3cca782a2ac7736c0612ef9ccd916de
nitwhiz <schonfeldandy@gmail.com>
2023-04-09 23:32:12 +0200

    fix stagecoach env substitution

--- docker/scripts/wrapper.sh
+++ docker/scripts/wrapper.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
 echo "using $SANE_SCAN_API_URL as sane-scan-api url"
+echo "using $STAGECOACH_API_URL as stagecoach-api url"
 
 cp /usr/share/nginx/html/env.json.dist /usr/share/nginx/html/env.json
+
 sed -i "s#SANE_SCAN_API_URL#$SANE_SCAN_API_URL#" /usr/share/nginx/html/env.json
+sed -i "s#STAGECOACH_API_URL#$STAGECOACH_API_URL#" /usr/share/nginx/html/env.json
 
 exec nginx -g "daemon off;"
